c476add59725084229b11bf3a37604fa62af1189
ReZuCoS <tsyganok.vasya@yandex.ru>
2022-11-03 04:06:16 +0300

    Исправлен баг подсчёта градусов из времени

--- SeminarTasks/Task3/ClocksDegreeCalculator.cs
+++ SeminarTasks/Task3/ClocksDegreeCalculator.cs
@@ -7,7 +7,7 @@
             if (hours < 0)
                 throw new ArgumentOutOfRangeException($"\"{nameof(hours)}\" value must be in 0-24 range");
 
-            int degrees = (hours * 30) - 360 * hours / 12;
+            int degrees = (hours * 30) - 360 * (hours / 12);
 
             return Math.Abs(360 * (degrees / 180) - degrees);
         }
