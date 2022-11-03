f2e260da773dcbc07660abcbbfe269b1fb470c30
Василий Цыганок <tsyganok.vasya@yandex.ru>
2022-11-03 18:10:14 +0300

    Выполнено задание 4

---
+++ SeminarTasks.Tests/DivisorsCounterTests.cs
@@ -0,0 +1,42 @@
+﻿using SeminarTasks.Task4;
+
+namespace SeminarTasks.Tests
+{
+    public class DivisorsCounterTests
+    {
+        [TestCase(-45, 12, 2)]
+        [TestCase(-5, 13, 1)]
+        [TestCase(-12, 0, 0)]
+        public void ThrowsOnLowerOrEqualZero(int n, int x, int y)
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () =>
+            DivisorsCounter.Count(n, x, y));
+        }
+
+        [TestCase(24, 4, 11)]
+        [TestCase(58, 6, 2)]
+        [TestCase(9, 12, 3)]
+        public void ThrowsOnNotPrimeX(int n, int x, int y)
+        {
+            Assert.Throws(typeof(ArgumentException), () =>
+            DivisorsCounter.Count(n, x, y));
+        }
+
+        [TestCase(829, 11, 15)]
+        [TestCase(60, 13, 25)]
+        [TestCase(7, 3, 4)]
+        public void ThrowsOnNotPrimeY(int n, int x, int y)
+        {
+            Assert.Throws(typeof(ArgumentException), () =>
+            DivisorsCounter.Count(n, x, y));
+        }
+
+        [TestCase(20, 3, 5, 8)]
+        [TestCase(30, 5, 7, 9)]
+        [TestCase(10, 3, 5, 4)]
+        public void CheckDivisorsCount(int n, int x, int y, int expectedDivisiorsCount)
+        {
+            Assert.That(DivisorsCounter.Count(n, x, y), Is.EqualTo(expectedDivisiorsCount));
+        }
+    }
+}
---
+++ SeminarTasks/Task4/DivisorsCounter.cs
@@ -0,0 +1,43 @@
+﻿/* Expression 4
+ * Найти количество чисел меньших N,
+ * которые имеют простые делители X или Y.
+ */
+
+namespace SeminarTasks.Task4
+{
+    public static class DivisorsCounter
+    {
+        public static int Count(int n, int x, int y)
+        {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException($"\"{nameof(n)}\" must be more than 0!");
+            
+            if (!IsPrime(x))
+                throw new ArgumentException($"\"{nameof(x)}\" must be a prime number!");
+
+            if (!IsPrime(y))
+                throw new ArgumentException($"\"{nameof(y)}\" must be a prime number!");
+
+            int divisorsCount = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if ((i % x == 0) || (i % y == 0))
+                    divisorsCount++;
+            }
+
+            return divisorsCount;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
